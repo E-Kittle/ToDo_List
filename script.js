3918169c08b9bcec4ce3bb37c7a55c53410e59e4
Elisabeth Kittle <elisabethkittle@gmail.com>
2021-04-17 10:42:12 -0400

    Created draft for JS

--- script.js
+++ script.js
@@ -7,16 +7,127 @@ const projectFactory = (title, items) => {
 }
 
 const projectManager = (() => {
+    //This stores the list of projects
     const projectList = [];
     
+    //Adds a new project to the list. Takes in the title of the list, creates a new project, and adds it to the list
+    const addProj = (title) => {
+        let proj = projectFactory(title, []);
+        projectList.push(proj);
+    };
 
-    return {};
+    const removeProj = () => {
+        //This removes a project from the list. Likely should take in the index of the project 
+    };
+
+    const updateProj = () => {
+        //This will be complicated. The only updating is to update an item in the list. Maybe I should take in the new item, splice out the old one, and add in the new one 
+        //I think that splicing allows us to replace an item so I can do this in one move. Alternatively, I could just update the updated portion but that would likely require excessive code, probably faster and 'cheaper' to just replace it. 
+    };
+
+    const newItem = () => {
+        //this takes in a new item object and adds it to the item array
+    };
+
+    const getProjects = () => {
+        return projectList;
+    };
+
+    return {addProj};
 })();
 
+const DOMManager = (() => {
 
-// const itemManager;
+    const newProject = (title) => {
+        if (_validateProj){
+            projectManager.addProj(title);
 
+        }
+        //Triggered by the 'add new project' button (has a preventDefault handler). Grabs the data from the form (do I need to use a validate function or can I just put a min char?), calls projectManager.addProj(title), calls saveData(), calls displayProjList(), clearProjForm(), 
+    };
 
+    const newItem = () => {
+        //This is triggered by the 'add new item' button w/ a preventDefault handler. validateItem(), projectManager.newItem(), clearItemForm(), displayItemList(), saveData()
+    };
+
+    const displayProjList = () => {
+        //Calls projectManager.getProjects() and saves to an array. 
+    };
+
+    const displayItemList = () => {
+        //First, calls projectManager.getProjects(). Then checks to see which project is currently selected, loops through the list of items for that project, and updates the DOM with the items. 
+    };
+
+    
+    const _validateProj = () => {
+        //This checks to ensure that the project doesn't already exist
+    };
+    
+    const _validateItem = () => {
+        //Validates the item form
+    };
+    
+    const _clearProjForm = () => {
+        //This clears the project form
+    };
+    
+    const _clearItemForm = () => {
+        //Clears the item form
+    };
+    
+    
+    const _saveData = () =>{
+        
+    };
+    
+    const loadData = () => {
+        //Triggered by a page reload
+    };
+
+    //For modifying projects - Add later
+    const updateProj = () => {
+        //This 
+    };
+
+    const updateItem = () => {
+
+    };
+
+    const removeProj = () => {
+
+    };
+
+    const removeItem = () => {
+
+    };
+
+    return {};
+
+});
+
+const overlay = (() => {
+    const openProjOverlay = () => {
+        document.querySelector('.overlay').style.display = "flex";
+        document.querySelector('.projectOverlay').style.display = "flex";
+    };
+
+    const closeProjOverlay = () => {
+        document.querySelector('.overlay').style.display = "none";
+        document.querySelector('.projectOverlay').style.display = "none";
+    };
+
+    const openItmOverlay = () => {
+        document.querySelector('.overlay').style.display = "flex";
+        document.querySelector('.itemOverlay').style.display = "flex";
+    };
+
+    const closeItmOverlay = () => {
+        document.querySelector('.overlay').style.display = "none";
+        document.querySelector('.itemOverlay').style.display = "none";
+    };
+
+    return {openProjOverlay, closeProjOverlay, closeItmOverlay, openItmOverlay};
+})();
 
 // Constants for DOM items
 
@@ -37,25 +148,24 @@ addNewProject.addEventListener('click', function(event) {
     console.log('Project added');
 });
 
+
+
+
 // For the Overlay - First two are for adding a new project and second two are for adding a new item
 openProjectOverlay.addEventListener('click', () => {
-    document.querySelector('.overlay').style.display = "flex";
-    document.querySelector('.projectOverlay').style.display = "flex";
+    overlay.openProjOverlay();
 });
 
 closeProjectOverlay.addEventListener('click', () => {
-    document.querySelector('.overlay').style.display = "none";
-    document.querySelector('.projectOverlay').style.display = "none";
+    overlay.closeProjOverlay();
 });
 
 openItemOverlay.addEventListener('click', () => {
-    document.querySelector('.overlay').style.display = "flex";
-    document.querySelector('.itemOverlay').style.display = "flex";
+    overlay.openItmOverlay();
 });
 
 closeItemOverlay.addEventListener('click', () => {
-    document.querySelector('.overlay').style.display = "none";
-    document.querySelector('.itemOverlay').style.display = "none";
+    overlay.closeItmOverlay();
 });
 
 
